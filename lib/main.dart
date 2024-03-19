301b946f5eec42935516f5590087e07b84fd1849
unknown <nafeesahamed.tanmoy@gmail.com>
2024-03-20 05:03:39 +0600

    cupertino_app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return CupertinoApp(
       home: HomeScreen(),
     );
   }
@@ -22,188 +23,38 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-        backgroundColor: Colors.blue,
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        leading: Icon(CupertinoIcons.home),
+        middle: Text('Home'),
+        trailing:
+            CupertinoSwitch(value: true, onChanged: (bool onChanged) => true),
       ),
-      body: Center(
+      child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'Home',
-              style: TextStyle(fontSize: 24),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SettingScreen()),
-                );
-              },
-              child: Text('Go to SettingScreen'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => OrderScreen()),
-                );
-              },
-              child: Text('Go to OrderScreen'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ProductScreen()),
-                );
-              },
-              child: Text('Go to ProductScrren'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class SettingScreen extends StatelessWidget {
-  const SettingScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('SettingScreen'),
-        backgroundColor: Colors.blue,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'SettingsScreen',
-              style: TextStyle(fontSize: 30),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(builder: (context) => HomeScreen()),
-                    (route) => false);
-              },
-              child: Text('Home'),
+            Text('Hello world'),
+            Text('Hello world'),
+            Text('Hello world'),
+            Text('Hello world'),
+            Text('Hello world'),
+            Text('Hello world'),
+            CupertinoButton(child: Text('gkjsfhd'), onPressed: () {}),
+            CupertinoButton.filled(child: Text('gkjsfhd'), onPressed: () {}),
+            CupertinoTextField(),
+            CupertinoTabBar(
+              items: [
+                BottomNavigationBarItem(
+                    label: 'jkd', icon: Icon(CupertinoIcons.home)),
+                BottomNavigationBarItem(
+                    label: 'jkd', icon: Icon(CupertinoIcons.home)),
+                BottomNavigationBarItem(
+                    label: 'jkd', icon: Icon(CupertinoIcons.home)),
+                BottomNavigationBarItem(
+                    label: 'jkd', icon: Icon(CupertinoIcons.home)),
+              ],
             )
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class OrderScreen extends StatelessWidget {
-  const OrderScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('SettingScreen'),
-        backgroundColor: Colors.blue,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'OrdersScreen',
-              style: TextStyle(fontSize: 30),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => SettingScreen()),
-                );
-              },
-              child: Text('Go to Settings'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text('Back to Home'),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ProductScreen extends StatelessWidget {
-  const ProductScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('ProductScreen'),
-        backgroundColor: Colors.brown,
-      ),
-      body: ListView.builder(
-        itemCount: 30,
-        itemBuilder: (context, index) {
-          return ListTile(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => ProductDetailsScreen(
-                    productName: index.toString(),
-                    price: 453.44,
-                  ),
-                ),
-              ).then((value) => print(value));
-            },
-            title: Text(index.toString()),
-            subtitle: Text('Product Details $index'),
-          );
-        },
-      ),
-    );
-  }
-}
-
-class ProductDetailsScreen extends StatelessWidget {
-  final String productName;
-  final double? price;
-  const ProductDetailsScreen(
-      {super.key, required this.productName, this.price});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Product Details'),
-        backgroundColor: Colors.amber,
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Text(
-              productName,
-              style: TextStyle(fontSize: 30),
-            ),
-            Text(price.toString()),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context, 'My Name is Tonmoy $productName');
-              },
-              child: Text('Back'),
-            ),
           ],
         ),
       ),
