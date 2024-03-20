17a683b011edaac687ddf19c5c1766b5bdde47be
unknown <nafeesahamed.tanmoy@gmail.com>
2024-03-20 17:51:03 +0600

    class7_2_cupertino_widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
+    return MaterialApp(
       home: HomeScreen(),
     );
   }
@@ -23,40 +23,71 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        leading: Icon(CupertinoIcons.home),
-        middle: Text('Home'),
-        trailing:
-            CupertinoSwitch(value: true, onChanged: (bool onChanged) => true),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        backgroundColor: Colors.blue,
       ),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Hello world'),
-            Text('Hello world'),
-            Text('Hello world'),
-            Text('Hello world'),
-            Text('Hello world'),
-            Text('Hello world'),
-            CupertinoButton(child: Text('gkjsfhd'), onPressed: () {}),
-            CupertinoButton.filled(child: Text('gkjsfhd'), onPressed: () {}),
-            CupertinoTextField(),
-            CupertinoTabBar(
-              items: [
-                BottomNavigationBarItem(
-                    label: 'jkd', icon: Icon(CupertinoIcons.home)),
-                BottomNavigationBarItem(
-                    label: 'jkd', icon: Icon(CupertinoIcons.home)),
-                BottomNavigationBarItem(
-                    label: 'jkd', icon: Icon(CupertinoIcons.home)),
-                BottomNavigationBarItem(
-                    label: 'jkd', icon: Icon(CupertinoIcons.home)),
-              ],
-            )
-          ],
-        ),
+      body: Column(
+        children: [
+          Switch(value: true, onChanged: (bool value) {}),
+          ElevatedButton(
+              onPressed: () {
+                showDialog(
+                    barrierDismissible: false,
+                    context: context,
+                    builder: (context) => AlertDialog(
+                          title: Text('Alert'),
+                          content: Text('You are in Danger'),
+                          actions: [
+                            TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text('Cancel')),
+                            TextButton(onPressed: () {}, child: Text('Okay'))
+                          ],
+                        ));
+              },
+              child: Text('Show dialog')),
+          ElevatedButton(
+              onPressed: () {
+                showAboutDialog(
+                    context: context,
+                    applicationName: "PikaPie",
+                    applicationVersion: "1.4.2",
+                    children: [
+                      Text('This Application is good for regular uses'),
+                    ]);
+              },
+              child: Text('Show About')),
+          ElevatedButton(
+              onPressed: () {
+                showModalBottomSheet(
+                  isDismissible: false,
+                  backgroundColor: Colors.grey.shade100,
+                  barrierColor: Colors.black54,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(26),
+                      topRight: Radius.circular(26),
+                    ),
+                  ),
+                  context: context,
+                  builder: (context) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Center(child: Text('This is Modal Bottom Sheet')),
+                        ],
+                      ),
+                    );
+                  },
+                );
+              },
+              child: Text('Show Bottom Sheet'))
+        ],
       ),
     );
   }
